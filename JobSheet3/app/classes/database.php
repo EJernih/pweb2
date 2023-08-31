c438a4de3c6b118f9e6ac3cade4f59df0ab67b24
Ejernih <jerniharifin@gmail.com>
2023-08-31 16:03:29 +0700

    nambah data

--- JobSheet3/app/classes/database.php
+++ JobSheet3/app/classes/database.php
@@ -1,12 +1,12 @@
 <?php
 
 class database{
+
     var $host="localhost";
     var $username="root";
     var $password="";
     var $db="akademik";
     var $koneksi;
-
 
     function __construct()
     {
@@ -14,15 +14,34 @@ class database{
     }
 
     function tampil_mahasiswa(){
-        $data=mysqli_query($this->koneksi, "select * from mahasiswa");
-        while($d= mysqli_fetch_array($data)){
+        $data=mysqli_query($this->koneksi,"select * from mahasiswa");
+        while($d = mysqli_fetch_array($data)){
             $hasil[]=$d;
         }
         return $hasil;
     }
 
-    function tambah_mhs($nim, $nama, $alamat)
+    function tambah_mhs($nim,$nama,$alamat){
+        mysqli_query($this->koneksi,"insert into mahasiswa (nim,nama,alamat) values('$nim','$nama','$alamat')");
+    
+    }
+
+    function edit($nim)
     {
-        mysqli_query($this->koneksi, "insert into mahasiswa($nim,$nama,$alamat) values('$nim','$nama','$alamat')");
+        $data = mysqli_query($this->koneksi, "select * from mahasiswa where nim='$nim'");
+        while ($d = mysqli_fetch_array($data)){
+            $hasil[]=$d;
+    }
+    return $hasil;
+    }
+
+    function update($nim, $nama, $alamat)
+    {
+        mysqli_query($this->koneksi, "update mahasiswa set nim='$nim',nama='$nama',alamat='$alamat' where nim='$nim'");
+    }
+
+    function hapus($nim)
+    {
+        mysqli_query($this->koneksi, "delete from mahasiswa where nim='$nim'");
     }
 }
--- JobSheet3/app/views/proses_mhs.php
+++ JobSheet3/app/views/proses_mhs.php
@@ -3,8 +3,13 @@ include '../classes/database.php';
 $db=new database();
 
 $aksi= $_GET['aksi'];
-if($aksi == "tambah"){
-    $db->tambah_mhs($_POST['nim'],$_POST['nama'],$_POST['alamat']);
+if ($aksi=="tambah") {
+    $db->tambah_mhs($_POST['nim'], $_POST['nama'], $_POST['alamat']);
+    header("location:tampil_mhs.php");
+}elseif($aksi == "update"){
+    $db->update($_POST['nim'],$_POST['nama'], $_POST['alamat']);
+    header("location:tampil_mhs.php");
+}elseif($aksi == "hapus"){
+    $db->hapus($_GET['id']);
     header("location:tampil_mhs.php");
 }
-?>
